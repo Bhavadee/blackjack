ff3ac215dd4e79090981e988ab9db33a972e02c0
bhvadeepreddy <bhavadeep2003@gmail.com>
2021-12-30 20:33:34 +0530

    blackjack

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 4
+let sum = firstCard + secondCard 
+let cards = [firstCard ,secondCard]
 let hasBlackJack = false
 let message = ""
 
 let mesg = document.getElementById("message-el")
 // 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el") 
-
+let cardsEl = document.getElementById("cards-el")
 function startGame()
 {
-      sumEl.textContent += sum
+  renderGame()
+}
+function renderGame()
+{
+      sumEl.textContent = "sum : " + sum
+      cardsEl.textContent = "Cards :";
+      for(let i=0;i<cards.length;i++){
+        cardsEl.textContent += cards[i] + " "
+      }
+      
     if (sum <= 20) {
-        message = "Do you want to draw a new card? "
+        message = "Do you want to draw a new card? 🤞🤦‍♂️"
      } else if (sum === 21) {
-         message = "Wohoo! You've got Blackjack! "
+         message = "Wohoo! You've got Blackjack! 🎉🙌"
          hasBlackJack = true
      } else {
-         message = "You're out of the game! "
+         message = "You're out of the game! 😥😥"
          isAlive = false
      }
    mesg.textContent = message
+}
+function newCard()
+{
+    
+    let card = 4
+    sum += card
+    cards.push(card)
+    renderGame()
 }
 
