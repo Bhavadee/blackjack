0f2f3c36aae7bb15fe33597448438281f4496756
bhvadeepreddy <bhavadeep2003@gmail.com>
2021-12-30 12:34:40 +0530

    blackjack game

---
+++ index.js
@@ -0,0 +1,16 @@
+let fristcard = 5
+let secondcard = 8
+let sum = fristcard+secondcard
+
+if(sum <= 20)
+{
+    console.log("Do you want to draw a new card? 🙂")
+}
+else if (sum == 21)
+{
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
+else
+{
+    console.log("You're out of the game! 😭")
+}
