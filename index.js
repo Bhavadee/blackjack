47ecfedde698ae64633afda4b526c5a96af54d9e
bhavdeep reddy <87595670+Bhavadee@users.noreply.github.com>
2021-12-31 21:42:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
-let player = {
-    name: "BHavadeep",
-    chips: 200
-}
+
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
 
-playerEl.textContent = player.name + ": $" + player.chips
+
+
 function getRandomCard() {
     let randomNumer = Math.floor( Math.random()*13 ) + 1
     if (randomNumer > 10) {
